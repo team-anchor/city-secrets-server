c41ed1c9e60bdc79f59f37cb927b295b2101ebb0
Mario Quintana <pyrros2000@gmail.com>
2018-09-17 11:54:56 -0700

    Auth e2e tests passing

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -38,4 +38,43 @@ describe('Auth API', () => {
                 assert.isDefined(body.token);
             });
     });
+
+    it('Can sign in a user', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'me@me.com',
+                password: '123'
+            })
+            .then(checkOk)
+            .then(({ body }) => {
+                assert.isDefined(body.token);
+            });
+    });
+
+    it('Fails on a wrong password', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'me@me.com',
+                password: 'bad'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password');
+            });
+    });
+
+    it('Cannot sign up with same email', () => {
+        return request
+            .post('/api/auth/signup')
+            .send({
+                email: 'me@me.com',
+                password: '123'
+            })
+            .then(res => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'Email already in use');
+            });
+    });
 });
