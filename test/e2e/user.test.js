2593e96ca6fbe9a1f667ed1b8a5db6a59c71fbf0
Mark Myers <markalope@gmail.com>
2018-09-17 14:28:03 -0700

    working on user e2e test

---
+++ test/e2e/user.test.js
@@ -0,0 +1,52 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+const { checkOk } = request;
+
+describe ('Users API', () => {
+
+    beforeEach(() => dropCollection('users'));
+    beforeEach(() => dropCollection('tours'));
+
+    let token;
+    beforeEach(() => {
+        return request
+            .post('/api/auth/signup')
+            .send({
+                email: 'me@me.com',
+                password: '123'
+            })
+            .then(checkOk)
+            .then(({ body }) => {
+                token = body.token;
+            });
+    });
+
+    function saveUser(user) {
+        return request
+            .post('/api/users')
+            .set('Authorization', token)
+            .send(user)
+            .then(checkOk)
+            .then(({ body }) => {
+                delete body.__v;
+                return body;
+            });
+    }
+
+    let joe;
+    beforeEach(() => {
+        return saveUser({
+            name: 'Joe Walker',  
+            email: 'email@example.org'
+        })
+            .then(data => {
+                joe = data;
+            });
+
+    });
+
+    it('Saves a user', () => {
+        assert.isOk(joe._id);
+    });
+});
