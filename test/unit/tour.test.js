7ec1b2d7def2ed206df00b1cf68df976e2fcb43d
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-17 12:20:42 -0700

    Write unit test for tour and verify it passes

--- test/unit/tour.test.js
+++ test/unit/tour.test.js
@@ -1,3 +1,36 @@
-// const { assert } = require('chai');
-// const Tour = require('../../lib/models/tour');
-// const { getErrors } = require('./helpers');
+const { assert } = require('chai');
+const { Types } = require('mongoose');
+const Tour = require('../../lib/models/tour');
+
+describe('Tour model', () => {
+    it('validates a good model', () => {
+        const data = {
+            name: 'SE Murals',
+            description: 'The best murals in SE PDX',
+            userId: Types.ObjectId(),
+            stops: [
+                {
+                    location: {
+                        address: '1300 SE Stark St, Portland OR, 97214',
+                        picture: 'https://www.randomimage.com',
+                        caption: 'This is where you start the tour!'
+                    }
+                },
+                {
+                    location: {
+                        address: '923 SE 7th Ave, Portland OR 97214',
+                        picture: 'https://www.randomimage.com',
+                        caption: 'This is where you finish the tour!'
+                    }
+                }
+            ]
+        };
+
+        const tour = new Tour(data);
+        const json = tour.toJSON();
+        delete json._id;
+        json.stops.forEach(s => delete s._id);
+
+        assert.deepEqual(json, data);
+    });
+});
